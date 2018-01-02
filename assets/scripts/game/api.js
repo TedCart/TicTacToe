482521a8c911e4b1faceaa6cacf731d55b2b89a8
TedCart <TedCart@gmail.com>
2018-01-02 13:31:41 -0500

    Added conditions to game API calls
    If a user is not logged in, the page will no longer call the API (a feature which previously filled the console with error messages).

--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -4,46 +4,58 @@ const config = require('../config')
 const store = require('../store')
 
 const newGame = () => {
-  return $.ajax({
-    url: config.apiOrigin + '/games',
-    method: 'POST',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
+  // Verifies a user is logged in before creating a new game
+  if (store.user) {
+    return $.ajax({
+      url: config.apiOrigin + '/games',
+      method: 'POST',
+      headers: {
+        Authorization: 'Token token=' + store.user.token
+      }
+    })
+  }
 }
 
 const getGame = () => {
-  return $.ajax({
-    url: config.apiOrigin + '/games/' + store.game.id,
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
+  // Verifies a user is logged in before getting a game
+  if (store.user) {
+    return $.ajax({
+      url: config.apiOrigin + '/games/' + store.game.id,
+      method: 'GET',
+      headers: {
+        Authorization: 'Token token=' + store.user.token
+      }
+    })
+  }
 }
 
 const getAllGames = () => {
-  return $.ajax({
-    url: config.apiOrigin + '/games',
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
+  // Verifies a user is logged in before getting all games
+  if (store.user) {
+    return $.ajax({
+      url: config.apiOrigin + '/games',
+      method: 'GET',
+      headers: {
+        Authorization: 'Token token=' + store.user.token
+      }
+    })
+  }
 }
 
 const patchOffsite = function (data) {
-  const json = JSON.stringify(data)
-  return $.ajax({
-    url: config.apiOrigin + '/games/' + store.game.id,
-    method: 'PATCH',
-    contentType: 'application/json',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    },
-    data: json
-  })
+  // Verifies a user is logged in before updating a game
+  if (store.user) {
+    const json = JSON.stringify(data)
+    return $.ajax({
+      url: config.apiOrigin + '/games/' + store.game.id,
+      method: 'PATCH',
+      contentType: 'application/json',
+      headers: {
+        Authorization: 'Token token=' + store.user.token
+      },
+      data: json
+    })
+  }
 }
 
 module.exports = {
